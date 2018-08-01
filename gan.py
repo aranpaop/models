677c8c5b2a2804d7457a15f7fd6ca6118a801d2e
aranpaop <39664534+aranpaop@users.noreply.github.com>
2018-08-01 15:03:55 +0800

    Add files via upload

---
+++ gan.py
@@ -0,0 +1,40 @@
+from keras import Sequential
+from keras.layers import Dense, Activation, BatchNormalization, Reshape, UpSampling2D, Conv2D, MaxPooling2D, Flatten
+
+
+def generator_model():
+    model = Sequential()
+
+    model.add(Dense(input_dim=100, output_dim=1024))
+    model.add(Activation('relu'))
+    model.add(Dense(128*7*7))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(Reshape((7, 7, 128), input_shape=(128*7*7)))
+    model.add(UpSampling2D(size=(2, 2)))
+    model.add(Conv2D(64, (5, 5), padding='same'))
+    model.add(Activation('relu'))
+    model.add(UpSampling2D(size=(2, 2)))
+    model.add(Conv2D(1, (5, 5), padding='same'))
+    model.add(Activation('tanh'))
+
+    return model
+
+
+def discriminator_model():
+    model = Sequential()
+
+    model.add(Conv2D(64, (5, 5), padding='same', input_shape=(28, 28, 1)))
+    model.add(Activation('tanh'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(128, (5, 5)))
+    model.add(Activation('tanh'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(1024))
+    model.add(Activation('tanh'))
+    model.add(Dense(1))
+    model.add(Activation('sigmoid'))
+
+    return model
+
