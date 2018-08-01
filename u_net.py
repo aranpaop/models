890490ebe9c6a6b3761b34316e553692bd9efb09
aranpaop <39664534+aranpaop@users.noreply.github.com>
2018-08-01 14:40:00 +0800

    Add files via upload

---
+++ u_net.py
@@ -0,0 +1,51 @@
+from keras.layers import Conv2D, MaxPooling2D, Conv2DTranspose, Concatenate
+from keras import Input, Model
+
+
+def get_unet():
+    input = Input(shape=(128, 128, 1))
+
+    c1 = Conv2D(8, (3, 3), activation='relu', padding='same')(input)
+    c1 = Conv2D(8, (3, 3), activation='relu', padding='same')(c1)
+    p1 = MaxPooling2D((2, 2))(c1)
+
+    c2 = Conv2D(16, (3, 3), activation='relu', padding='same')(p1)
+    c2 = Conv2D(16, (3, 3), activation='relu', padding='same')(c2)
+    p2 = MaxPooling2D((2, 2))(c2)
+
+    c3 = Conv2D(32, (3, 3), activation='relu', padding='same')(p2)
+    c3 = Conv2D(32, (3, 3), activation='relu', padding='same')(c3)
+    p3 = MaxPooling2D((2, 2))(c3)
+
+    c4 = Conv2D(64, (3, 3), activation='relu', padding='same')(p3)
+    c4 = Conv2D(64, (3, 3), activation='relu', padding='same')(c4)
+    p4 = MaxPooling2D((2, 2))(c4)
+
+    c5 = Conv2D(128, (3, 3), activation='relu', padding='same')(p4)
+    c5 = Conv2D(128, (3, 3), activation='relu', padding='same')(c5)
+
+    u6 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c5)
+    u6 = Concatenate(axis=3)([u6, c4])
+    c6 = Conv2D(64, (3, 3), activation='relu', padding='same')(u6)
+    c6 = Conv2D(64, (3, 3), activation='relu', padding='same')(c6)
+
+    u7 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c6)
+    u7 = Concatenate(axis=3)([u7, c3])
+    c7 = Conv2D(32, (3, 3), activation='relu', padding='same')(u7)
+    c7 = Conv2D(32, (3, 3), activation='relu', padding='same')(c7)
+
+    u8 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(c7)
+    u8 = Concatenate(axis=3)([u8, c2])
+    c8 = Conv2D(16, (3, 3), activation='relu', padding='same')(u8)
+    c8 = Conv2D(16, (3, 3), activation='relu', padding='same')(c8)
+
+    u9 = Conv2DTranspose(8, (2, 2), strides=(2, 2), padding='same')(c8)
+    u9 = Concatenate(axis=3)([u9, c1])
+    c9 = Conv2D(8, (3, 3), activation='relu', padding='same')(u9)
+    c9 = Conv2D(8, (3, 3), activation='relu', padding='same')(c9)
+
+    output = Conv2D(1, (1, 1), activation='sigmoid')(c9)
+
+    model = Model(inputs=[input], outputs=[output])
+
+    return model
